53b6ac9c822251ab25029ab1774aab6c95ffcbfe
sammyrhymes <samuelmwendwa5996@gmail.com>
2024-07-31 18:36:48 +0300

    adding query string parameters

--- server.js
+++ server.js
@@ -32,6 +32,24 @@ app.get('/api/products/:productID', (req,res)=>{
     res.json(singleProduct)
 })
 
+app.get('/api/v1/query', (req,res) =>{
+    let sortedProducts = [...products]
+    // console.log(req.query)              
+    const {search, limit} = req.query
+    if (search){
+        sortedProducts = sortedProducts.filter((product) =>{
+            return product.productName.startsWith(search)
+        })
+    }
+    if (limit){
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if (sortedProducts.length >1){
+        return res.status(200).json({succes: true, data: []})
+    }
+    res.status(200).json(sortedProducts)
+})
+
 app.listen(5000, () => {
     console.log(`server runnning on http://localhost:5000`)
 })
